37d1b841525dd777119e592e33876a743d0edc79
crimsonronin <josh.stuart@zoopcommerce.com>
2015-04-18 18:03:58 +1000

    Added RN backend service tracking.

--- app/index.js
+++ app/index.js
@@ -4,8 +4,9 @@
 
 var _ = require('lodash');
 var common = require('evergram-common');
+var newrelic = require('newrelic');
 var logger = common.utils.logger;
-var config = require('./config');
+var retryWaitTime = require('./config').retryWaitTime * 1000;
 var consumer = require('./consumer');
 
 //init db
@@ -14,18 +15,24 @@ common.db.connect();
 function run() {
     logger.info('Checking print queue');
     try {
-        consumer.consume().then(function () {
+        consumer.consume().then(newrelic.createBackgroundTransaction('jobs:process-queue', function (message) {
+            newrelic.endTransaction();
+            if (!_.isEmpty(message)) {
+                logger.info(message);
+            }
             logger.info('Completed checking print queue');
-            setTimeout(run, config.retryWaitTime * 1000);
-            logger.info('Waiting ' + config.retryWaitTime + ' seconds before next check');
-        }).fail(function (err) {
+            logger.info('Waiting ' + (retryWaitTime / 1000) + ' seconds before next check');
+            setTimeout(run, retryWaitTime);
+        })).fail(function (err) {
+            newrelic.endTransaction();
             if (!_.isEmpty(err)) {
                 logger.info(err);
             }
-            logger.info('Waiting ' + config.retryWaitTime + ' seconds before next check');
-            setTimeout(run, config.retryWaitTime * 1000);
+            logger.info('Waiting ' + (retryWaitTime / 1000) + ' seconds before next check');
+            setTimeout(run, retryWaitTime);
         }).done();
     } catch (err) {
+        setTimeout(run, retryWaitTime);
         logger.error(err);
     }
 }
